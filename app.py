fc448c89e8774c443bbe71c74ca138ed58b148b2
aya2004-it <ayahedresmoha@gmail.com>
2026-04-30 00:39:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 import streamlit as st
-from supabase import create_client
 
-# 🔑 بيانات Supabase
-url = "https://dpttbbaepbgyaaedingk.supabase.co"
-key = "sb_publishable_E5rLG4YeNSbObg860425pQ_gUngCzVf"
-
-supabase = create_client(url, key)
-
-# 🌱 عنوان الموقع
-st.title("🌱 Napta Plants")
-
-# 📥 جلب البيانات
 data = supabase.table("plants").select("*").execute().data
 
-# 🔍 بحث
-search = st.text_input("🔍 Search for plant")
+st.title("🌱 Napta Plants")
 
-# 🪴 عرض النباتات
+images = {
+    "Aloe vera": "https://images.unsplash.com/photo-1501004318641-b39e6451bec6",
+}
+
+unique_plants = {}
+
 for plant in data:
-    if search.lower() in plant["Plant_ID"].lower():
-        st.markdown(f"## 🌿 {plant['Plant_ID']}")
-        st.write("💧 Water every:", plant["Watering_Frequency_days"], "days")
-        st.write("🌞 Sun:", plant["Sunlight_Exposure"])
-        st.write("🪴 Health Score:", plant["Health_Score"])
-        st.write("---")
+    name = plant["Plant_ID"]
+    if name not in unique_plants:
+        unique_plants[name] = plant
+
+for plant in unique_plants.values():
+    st.subheader("🌿 " + plant["Plant_ID"])
+    
+    st.image(images.get(plant["Plant_ID"], "https://via.placeholder.com/200"))
+    
+    st.write("💧 Water every:", plant["Watering_Frequency_days"], "days")
+    st.write("🌞 Sun:", plant["Sunlight_Exposure"])
+    st.write("🪴 Health Score:", plant["Health_Score"])
+    
+    st.write("---")
