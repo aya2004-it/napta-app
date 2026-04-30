3577a4ac439b870156298f7c60d2a8ad3ab4e978
aya2004-it <ayahedresmoha@gmail.com>
2026-04-30 07:06:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
 import streamlit as st
 from supabase import create_client
 
-# 🔐 Supabase credentials
-url = "https://dpttbbaepbgyaaedingk.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFidmVleHFta3hvamJ3aHp5cXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MDE2ODksImV4cCI6MjA5Mjk3NzY4OX0.kq2RAY0Wt8Q5Mjn3lB42L5lZCz0wxKZNX0fZLUvEf74"
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(url, key)
 
-st.title("🌱 Napta Plants Dashboard")
+data = supabase.table("plants").select("*").execute().data
 
-try:
-    response = supabase.table("plants").select("*").execute()
-    data = response.data
-
-    if not data:
-        st.warning("No plants found 😢")
-    else:
-        for plant in data:
-            st.subheader(f"🌿 {plant.get('Plant_ID', 'Unknown')}")
-
-            st.write("💧 Water:", plant.get("Watering_Frequency_days"), "days")
-            st.write("🌞 Sun:", plant.get("Sunlight_Exposure"))
-            st.write("🪴 Health Score:", plant.get("Health_Score"))
-
-            st.write("---")
-
-except Exception as e:
-    st.error("🔥 Error happened:")
-    st.write(e)
+st.write(data)
